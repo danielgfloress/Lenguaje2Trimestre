1c1f3644e01b2f9eeddc443431379832c1c2a351
Reckles1010 <bertadiazflores@gmail.com>
2026-02-10 14:24:00 +0100

    añadiendo html de pagina tienda

---
+++ carrusel.js
@@ -0,0 +1,45 @@
+setTimeout(function() {
+    alert("INAZUMA ELEVEN");
+}, 500);
+
+const track = document.querySelector(".tienda__track");
+const slides = document.querySelectorAll(".tienda__carrusel");
+const prevBtn = document.getElementById("prevBtn");
+const nextBtn = document.getElementById("nextBtn");
+const dotsContainer = document.getElementById("dotsContainer");
+
+let index = 0;
+
+slides.forEach((_, i) => {
+    const dot = document.createElement("span");
+    dot.classList.add("carrusel__point");
+    if (i === 0) dot.classList.add("active");
+    dot.addEventListener("click", () => goToSlide(i));
+    dotsContainer.appendChild(dot);
+});
+
+const dots = document.querySelectorAll(".carrusel__point");
+
+function updateCarousel() {
+    track.style.transform = `translateX(-${index * 100}%)`;
+    dots.forEach(dot => dot.classList.remove("active"));
+    dots[index].classList.add("active");
+}
+
+function goToSlide(i) {
+    index = i;
+    updateCarousel();
+}
+
+function nextSlide() {
+    index = (index + 1) % slides.length;
+    updateCarousel();
+}
+
+function prevSlide() {
+    index = (index - 1 + slides.length) % slides.length;
+    updateCarousel();
+}
+
+nextBtn.addEventListener("click", nextSlide);
+prevBtn.addEventListener("click", prevSlide);
